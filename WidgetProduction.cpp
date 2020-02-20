2ffd826d8eb06a75f4f9b3dcd8dd6b86370258f6
Alex <32425085+alexshakouri@users.noreply.github.com>
2020-02-20 15:36:50 -0800

    Fully working prototype of widgetproduction!

--- WidgetProduction.cpp
+++ WidgetProduction.cpp
@@ -13,9 +13,9 @@ using namespace std;
 
  struct widget_def{
     string id;
-    string source;
-    chrono::system_clock::time_point time; //ctime doesn't show less than second
-    bool broken;
+    string producer;
+    chrono::system_clock::time_point timeInit; //ctime doesn't show less than second
+    bool isBroken;
 };
 
  class widget {
@@ -26,13 +26,13 @@ using namespace std;
 
     widget(string producer, int id_length, bool broken_widget) {
         //Define ID as 16-16 random characters
-        string temp;
         char randLetNum;
-        int LetOrNum, IDSeperator = id_length/2 - 1; //Add '-' halfway in the ID
+        int IDSeperator = id_length/2 - 1; //Add '-' halfway in the ID
+        bool LetOrNum;
 
         for (int i = 0; i < id_length; i++) {
-            LetOrNum = rand() % 2;
-            if (LetOrNum == 0) {
+            LetOrNum = static_cast<bool>(rand() % 2);
+            if (LetOrNum) {
                 randLetNum = '0' + rand() % 10; //Random number
             }
             else {
@@ -45,31 +45,32 @@ using namespace std;
             }
         }
         //Define source
-        this->w1.source = producer;
+        this->w1.producer = producer;
         //Define time
-        this->w1.time = chrono::system_clock::now();         
+        this->w1.timeInit = chrono::system_clock::now();         
         //Decide if this widget is broken
-        this->w1.broken = broken_widget;
+        this->w1.isBroken = broken_widget;
     }
 
-    string print_id() {
+    string get_id() {
         return (this->w1).id;
     }
 
-    string print_producer() {
-        return (this->w1).source;
+    string get_producer() {
+        return (this->w1).producer;
     }
 
-    string print_time() {
-        struct tm currTime; //struct zero-initializes currTime
-        time_t timeCurrent = chrono::system_clock::to_time_t((this->w1).time);
-        localtime_s(&currTime, &timeCurrent); //convert time since epoch to local time
+    //Get time in string format HH:MM:SS to output
+    string get_time() {
+        struct tm currTime = {}; //zero-initializes currTime
+        time_t timeCurrent = chrono::system_clock::to_time_t((this->w1).timeInit);
+        localtime_s(&currTime, &timeCurrent); //convert [time since epoch] to [local time]
         string time = to_string(currTime.tm_hour) + ':' + to_string(currTime.tm_min) + ':' + to_string(currTime.tm_sec);
         return time;
     }
 
-    string print_broken() {
-        if (this->w1.broken) {
+    string get_broken() {
+        if (this->w1.isBroken) {
             return "true";
         }
         else {
@@ -77,27 +78,31 @@ using namespace std;
         }
     }
 
-    chrono::duration<double> time_consume(chrono::system_clock::time_point time_consumed) {
-        return time_consumed - (this->w1.time);
+    chrono::duration<double> get_time_consumed(chrono::system_clock::time_point time_consumed) {
+        return time_consumed - (this->w1.timeInit);
     }
  };
 
 void produce_widget(vector<widget*>&, string, int, int, int&, int);
 void consume_widget(vector<widget*>&,string, bool&);
 
-mutex mtx;
+mutex mtx_produce;
+mutex mtx_consume;
+mutex mtx_print;
+mutex mtx_broken;
 
 int main()
 {
+    //TODO: Since I am running multiple functions the same second I need more fine grain on time
     srand(time(NULL)); //random number/letter generator
 
-    int idLength = 32;
-    int numProducers = 1;
-    int numConsumers = 1;
+    int widgetIdLength = 32;
+    int numProducers = 10; //Right now can only have 2 producers max hmmm
+    int numConsumers = 5;
     int maxWidgets = 50;
-    int brokenWidget = 1; //Which produced widget should be broken
-    int widgetNum = 0;
-    bool isBroken = false;
+    int brokenWidget = 2; //Which produced widget should be broken
+    int totWidgetsCreated = 0;
+    bool isBrokenWidget = false;
     vector<widget*> createdWidgets;
     createdWidgets.reserve(maxWidgets);
     vector<thread> tProducers;
@@ -105,17 +110,17 @@ int main()
 
     for (int i = 0; i < numProducers; i++) {
         string producerNum = "producer_" + to_string(i);
-        thread t_p(produce_widget, ref(createdWidgets), producerNum, idLength, brokenWidget, ref(widgetNum), maxWidgets);
+        thread t_p(produce_widget, ref(createdWidgets), producerNum, widgetIdLength, brokenWidget, ref(totWidgetsCreated), maxWidgets);
         tProducers.push_back(move(t_p));
     }
     
     for (int i = 0; i < numConsumers;i++) {
         string consumerNum = "consumer_" + to_string(i);
-        thread t_c(consume_widget, ref(createdWidgets), consumerNum, ref(isBroken));
+        thread t_c(consume_widget, ref(createdWidgets), consumerNum, ref(isBrokenWidget));
         tConsumers.push_back(move(t_c));
     }
     
-    //After we consume the broken widget we should join all the threads end the program 
+    //After we consume the broken widget we should join all the threads and end the program 
     for (auto it = tProducers.begin(); it != tProducers.end(); ++it) {
         (*it).join();
     }
@@ -127,57 +132,56 @@ int main()
     for (auto i = createdWidgets.begin(); i != createdWidgets.end(); i++) {
         delete *i;
     }
-    
 
 }
 
-void produce_widget(vector<widget*> &createdWidgets, string producer,int idLength, int brokenWidget, int &widgetNum, int maxWidgets) {
-    while (widgetNum < maxWidgets) {
-        createdWidgets.push_back(new widget(producer, idLength, widgetNum == brokenWidget));
-        widgetNum++;
+void produce_widget(vector<widget*> &createdWidgets, string producer,int idLength, int brokenWidget, int &totWidgetsCreated, int maxWidgets) {
+    while (totWidgetsCreated < maxWidgets) {
+        mtx_produce.lock();
+        createdWidgets.push_back(new widget(producer, idLength, totWidgetsCreated == brokenWidget));
+        totWidgetsCreated++;
+        mtx_produce.unlock();
+        //Add a delay to let other producers get a chance to create widgets
+        this_thread::sleep_for(chrono::nanoseconds(1000)); //This seems to work for now anything lower and it is not a good enough delay
     }
 }
 
-void consume_widget(vector<widget*> &createdWidgets, string consumer, bool &isBroken) {
+void consume_widget(vector<widget*> &createdWidgets, string consumer, bool &isBrokenWidget) {
     
-    bool brokenWidget = false;
-
-    do {
-
+    while(!isBrokenWidget){
+        
+        //Check that there are widgets for the consumer to consume
+        mtx_consume.lock();
         if (createdWidgets.empty()) {
+            mtx_consume.unlock(); //Make sure to unlock since I will be continuing here
             continue;
         }
         //Take the widget out of the vector when consuming it (so the same widget doesn't get consumed)
-        widget* w = createdWidgets.back();
+        widget w = *createdWidgets.back();
         createdWidgets.pop_back();
+        mtx_consume.unlock();
 
-        string widgetOutput = "[id=" + w->print_id() +
-            " source=" + w->print_producer() +
-            " time=" + w->print_time() +
-            " broken=" + w->print_broken() + "]";
+        string widgetOutput = "[id=" + w.get_id() +
+            " source=" + w.get_producer() +
+            " time=" + w.get_time() +
+            " broken=" + w.get_broken() + "]";
 
-        if (w->print_broken() == "false") {
+        if (w.get_broken() == "false") {
             //lock the output so they don't get scrambled with other consumers
-            mtx.lock();
+            mtx_print.lock();
             cout << consumer << " consumes " << widgetOutput << " in " 
-                << w->time_consume(chrono::system_clock::now()).count() << "s time\n";
-            mtx.unlock();
+                << w.get_time_consumed(chrono::system_clock::now()).count() << "s time\n";
+            mtx_print.unlock();
         }
         else {
-            isBroken = true;
+            mtx_broken.lock();
+            isBrokenWidget = true;
+            mtx_broken.unlock();
+            //Need to lock print too otherwise broken widget print can be inside the unbroken widget print
+            mtx_print.lock();
             cout << consumer << " found a broken widget " << widgetOutput << " -- stopping production\n";
+            mtx_print.unlock();
         }
-    } while (!isBroken);
+    }
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
